d86c77fc3cd8b6cac30e23b75fd9ba985deafb8c
Natalia Bohdanova <65080131+Natta-Sha@users.noreply.github.com>
2023-01-01 19:53:05 +0100

    basic HTML

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This project is created by Natta-Sha and is open-sourced{" "}
-        <a
-          href="https://github.com/Natta-Sha/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Link to Github project
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          {" "}
+          This project is created by Natta-Sha and is open-sourced{" "}
+          <a
+            href="https://github.com/Natta-Sha/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Link to Github project
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
