745ba98a7ef44816debe3610c1ae190166067fd9
Natalia Bohdanova <65080131+Natta-Sha@users.noreply.github.com>
2023-01-01 18:50:43 +0100

    Added Github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project is created by Natta-Sha and is open-sourced{" "}
+        <a
+          href="https://github.com/Natta-Sha/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Link to Github project
+        </a>
+      </footer>
+    </div>
+  );
 }
