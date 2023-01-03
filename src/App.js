32e2833e073d246835efaadbf04d3d8b094ce624
Natalia Bohdanova <65080131+Natta-Sha@users.noreply.github.com>
2023-01-03 23:13:01 +0100

    Added API intregration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           {" "}
           This project is created by Natta-Sha and is open-sourced{" "}
